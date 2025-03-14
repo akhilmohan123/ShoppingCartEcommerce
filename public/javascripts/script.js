73712b4a1dafe8d2d846bb9a1627daa69ea279e3
akhilmohan123 <akhilmohan8547@gmail.com>
2025-03-14 23:35:25 +0530

    implemented logic to show notification while clicking add to cart on individual product

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,25 @@
-function addtocart(proid) {
-  $.ajax({
-    url: "/cart/" + proid,
-    method: "get",
-    success: (response) => {
-      console.log("Server response:", response); // Debugging line
-      if (response.status) {
-        let count = $("#cart-count").html();
-        count = parseInt(count) + 1;
+// function addtocart(proid) {
+//   $.ajax({
+//     url: "/cart/" + proid,
+//     method: "get",
+//     success: (response) => {
+//       console.log("Server response:", response); // Debugging line
+//       if (response.status) {
+//         let count = $("#cart-count").html();
+//         count = parseInt(count) + 1;
 
-        $("#cart-count").html(count);
-        alert("Added");
-      }
-    },
-    error: (xhr, status, error) => {
-      if (xhr.status === 401) {
-        window.location.href = "/login"; // Redirect to login page
-      } else {
-        console.error("Error:", error);
-      }
-    },
-  });
-}
+//         $("#cart-count").html(count);
+//         alert("Added");
+//       }
+//     },
+//     error: (xhr, status, error) => {
+//       if (xhr.status === 401) {
+//         window.location.href = "/login"; // Redirect to login page
+//       } else {
+//         console.error("Error:", error);
+//       }
+//     },
+//   });
+// }
 
 
